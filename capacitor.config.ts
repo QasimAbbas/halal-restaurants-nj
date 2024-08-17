e01cc99d35f605232da6893d73293305511a87c2
Qasim Abbas <qasimabbas52@gmail.com>
2024-08-17 19:11:04 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Halal Restaurants NJ',
+  webDir: 'www'
+};
+
+export default config;
